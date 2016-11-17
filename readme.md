d2fb7e4be861d3dea99c78d1833c0e2ff16026c6
Shaws <seanrichardparker@gmail.com>
2016-11-16 23:10:44 -0500

    Fixing search and list functions

---
+++ readme.md
@@ -0,0 +1,9 @@
+##
+Finally figured out the man cheatsheet
+
+
+TO DO:
+- give this thing a wrapper and some HTML.
+- Make use of colorize (I guess)
+- Convert this thing to HAML (because why not)
+- Style it up good.
--- server.rb
+++ server.rb
@@ -14,33 +14,14 @@ get '/list' do
   erb :list
 end
 
-post '/cmd' do
+get '/result' do
   "You looked up #{params[:cmd]}."
-  `man #{params[:cmd]}`
+  `man #{params[:cmd]}`.to_s
 end
 
-post '/list' do
-  case object
-  when params[:touch]
-    "You looked up #{params[:touch]}."
-    `man #{params[:touch]}`
-  when params[:cp]
-    "You looked up #{params[:cp]}."
-    `man #{params[:cp]}`
-  when params[:mkdir]
-    "You looked up #{params[:mkdir]}."
-    `man #{params[:mkdir]}`
-  when params[:nano]
-    "You looked up #{params[:nano]}."
-    `man #{params[:nano]}`
-  when params[:less]
-    "You looked up #{params[:less]}."
-    `man #{params[:less]}`
-  when params[:vim]
-    "You looked up #{params[:vim]}."
-    `man #{params[:vim]}`]
-  end
-
+post '/result' do
+  "You looked up #{params[:cmd]}."
+  `man #{params[:cmd]}`.to_s
 end
 
 
